b2a2a985f4ac5252bf8891ca19f0854822e93db3
Denitsa Stanchewa <denitsa_stanchewa@outlook.com>
2025-11-17 15:04:45 +0200

    new

--- Final Project/main.py
+++ Final Project/main.py
@@ -1,7 +1,8 @@
-from data_load import load_collectors, load_customers, load_open_invoices
+from data_load import load_collectors, load_customers, load_open_invoices, convert_to_usd
 
 collectors = load_collectors("Collectors.xlsx")
 customers = load_customers("Customers.xlsx")
 open_invoices = load_open_invoices("open invoices.xlsx")
-open_invoices.convert_to_usd()
+for invoice in open_invoices:
+    invoice["amount_usd"] = convert_to_usd(invoice["amount"], invoice["currency"])
 
