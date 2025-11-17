c2b56547b2bd0922f6d954b04a419e91882fcaef
Denitsa Stanchewa <denitsa_stanchewa@outlook.com>
2025-11-17 14:13:53 +0200

    new

--- Final Project/data_load.py
+++ Final Project/data_load.py
@@ -2,56 +2,58 @@ import pandas as pd
 from customer import Customer
 from collector import Collector
 from account_manager import AccountManager
-from invoice import Invoice
+from invoice import OpenInvoice
+from invoice import ClosedInvoice
 
 def load_customers(filename="customers.xlsx"):
     df = pd.read_excel(filename)
-    return [Customer(row["PayerNumber"], row["CustomerName"]) for _, row in df.iterrows()]
+    return [Customer(row["Payer number"], row["Customer Name"]) for _, row in df.iterrows()]
 
 def load_collectors(filename="collectors.xlsx"):
     df = pd.read_excel(filename)
-    return [Collector(row["CollectorName"], row["CollectorManager"]) for _, row in df.iterrows()]
+    return [Collector(row["Collector Full Name"], row["Manager"]) for _, row in df.iterrows()]
 
 def load_account_managers(filename="account_managers.xlsx"):
     df = pd.read_excel(filename)
-    return [AccountManager(row["ManagerName"], row["Business"], row["ManagedPayer"]) for _, row in df.iterrows()]
+    return [AccountManager(row["Account Manager"], row["Business segment"], row["Managed Payer"]) for _, row in df.iterrows()]
 
 def load_open_invoices(filename="open invoices.xlsx"):
     df = pd.read_excel(filename)
     return [
-        Invoice(
-            row["InvoiceID"],
-            row["CustomerID"],
-            row["CustomerName"],
-            row["InvoiceDate"],
-            row["DueDate"],
-            row["AmountDocCurrency"],
-            row["DocCurrency"],
+        OpenInvoice(
+            row["Document Number"],
+            row["Payer Number"],
+            row["Customer Name"],
+            row["Document Date"],
+            row["Net due date"],
+            row["Amount in doc. curr."],
+            row["Document currency"],
             row["Status"],
-            row["CoCode"],
-            row["ProfitCenter"],
+            row["Company Code"],
+            row["Profit Center"],
         )
         for _, row in df.iterrows()
     ]
 def load_closed_invoices(filename="closed invoices.xlsx"):
     df = pd.read_excel(filename)
     return [
-        Invoice(
-            row["InvoiceID"],
-            row["CustomerID"],
-            row["CustomerName"],
-            row["InvoiceDate"],
-            row["DueDate"],
-            row["AmountDocCurrency"],
-            row["DocCurrency"],
+        ClosedInvoice(
+            row["Document Number"],
+            row["Payer Number"],
+            row["Customer Name"],
+            row["Document Date"],
+            row["Net due date"],
+            row["Amount in doc. curr."],
+            row["Document currency"],
             row["Status"],
-            row["CoCode"],
-            row["ProfitCenter"],
+            row["Company Code"],
+            row["Profit Center"],
+            row["Payment date"],
         )
         for _, row in df.iterrows()
     ]
 
-def convert_to_USD(input_file="open invoices.xlsx", output_file="open invoices converted.xlsx"):
+def convert_to_usd(input_file="open invoices.xlsx", output_file="open invoices converted.xlsx"):
     # Exchange rates
     rates = {"USD": 1.00, "EUR": 1.16, "GBP": 1.32, "TRY": 0.024, "PLN":0.27, "DKK": 0.16}
 
--- Final Project/invoice.py
+++ Final Project/invoice.py
@@ -2,8 +2,10 @@ from datetime import datetime
 import pandas as pd
 import calendar
 
-class Invoice:
-    def __init__(self, invoice_id, customer_id,customer_name, invoice_date, due_date, amount, document_currency, status, co_code, profit_center):
+
+class OpenInvoice:
+    def __init__(self,status, invoice_id, customer_id, customer_name, invoice_date, due_date, amount, document_currency,
+                  co_code, profit_center):
         self.invoice_id = invoice_id
         self.customer_id = customer_id
         self.customer_name = customer_name
@@ -15,8 +17,26 @@ class Invoice:
         self.profit_center = profit_center
         self.invoice_date = invoice_date
         self.days_late = self.calculate_days_late()
-        self.last_day_of_the_current_month = self.find_the_last_day_current_month()
-        self.bucket = self.assign_to_overdue_bucket()
+        # self.last_day_of_the_current_month = self.find_the_last_day_current_month()
+        # self.bucket = self.assign_to_overdue_bucket()
+
+class ClosedInvoice:
+    def __init__(self, status, invoice_id, customer_id, customer_name, invoice_date, due_date, amount, document_currency,
+                  co_code, profit_center, payment_date):
+        self.invoice_id = invoice_id
+        self.customer_id = customer_id
+        self.customer_name = customer_name
+        self.due_date = pd.to_datetime(due_date).date()
+        self.amount = amount
+        self.currency = document_currency
+        self.status = status
+        self.co_code = co_code
+        self.profit_center = profit_center
+        self.invoice_date = invoice_date
+        self.payment_date = payment_date
+        self.days_late = self.calculate_days_late()
+        # self.last_day_of_the_current_month = self.find_the_last_day_current_month()
+        # self.bucket = self.assign_to_overdue_bucket()
 
     def find_the_last_day_current_month(self):
         today = datetime.today()
@@ -24,7 +44,7 @@ class Invoice:
         # monthrange() to gets the date range
         # year = 2025, month = 11
         result = calendar.monthrange(today.year, today.month)
-        day = result[1] - 1 # last day is not considered for the curr.month
+        day = result[1] - 1  # last day is not considered for the curr.month
         return day
 
     def calculate_days_late(self):
